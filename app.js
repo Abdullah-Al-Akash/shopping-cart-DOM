7b6a1cd2bf9ff0a18772fbc3967d671dfbcf09ea
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-08-18 02:36:44 +0600

    Update All feature With error

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ function updateCaseNumber(product, price, isIncrease) {
 
         const caseAmount = document.getElementById(product + '-amount');
         caseAmount.innerText = caseCountNumber * price;
+        calculateTotal()
+
+        //Checkout Button Active:
+        const totalAmountText = document.getElementById('total-amount').innerText;
+        const totalAmount = parseFloat(totalAmountText);
+        if (totalAmount == 0) {
+                document.getElementById('checkout').setAttribute('disabled', true);
+        }
+        else {
+                document.getElementById('checkout').removeAttribute('disabled');
+        }
 }
 
 // Set Increment Case:
@@ -45,3 +56,27 @@ document.getElementById("phone-minus").addEventListener('click', function () {
 document.getElementById("phone-count").addEventListener('change', function () {
         updateCaseNumber('phone', 1219, false);
 })
+
+//Product Equation:
+function updateProductDetails(product) {
+        const productInput = document.getElementById(product + "-count");
+        const productCount = parseInt(productInput.value);
+        return productCount;
+}
+
+//Calculate Total Price:
+function calculateTotal() {
+        const phoneAmount = updateProductDetails('phone') * 1219;
+        const caseAmount = updateProductDetails('case') * 59;
+        const subTotal = phoneAmount + caseAmount;
+        const tax = subTotal / 100;
+        const totalAmount = subTotal + tax;
+        //Update Sub Total Amount:
+        document.getElementById('sub-total').innerText = subTotal;
+        //update Tax Amount:
+        document.getElementById('tax').innerText = tax;
+        //Update Total Amount:
+        document.getElementById('total-amount').innerText = totalAmount;
+
+}
+
